cbb6d4ae1cc0b5cc7429d62cab494c5328074207
marcos <marcosmvg77@gmail.com>
2021-09-06 13:01:07 -0300

    feature(input): adicionando componente de input

---
+++ src/Components/Input/index.tsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { ComponentInput } from "../../Types";
+
+const StyledContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  padding: 5px;
+  width: 350px;
+  border-bottom: 3px solid var(--Main-Black);
+  i {
+    font-size: 30px;
+    margin-right: 1rem;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+  select,
+  input {
+    flex: 1;
+    width: 100%;
+    border: none;
+    background: transparent;
+    font-family: var(--Font-Saira);
+    color: var(--Second-Black);
+    font-size: 1rem;
+    font-weight: bold;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+  option {
+    font-family: var(--Font-Saira);
+    background: transparent;
+    color: var(--Second-Black);
+    font-size: 1rem;
+    font-weight: bold;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+export const Input = ({
+  children,
+  IsSelect,
+  icon,
+  register,
+  name,
+  error,
+  type,
+  placeholder,
+  ...rest
+}: ComponentInput) => {
+  return (
+    <StyledContainer>
+      {IsSelect ? (
+        <>
+          {icon}
+          <select
+            //   {...register(name)}
+            {...rest}
+          >
+            {children}
+          </select>
+        </>
+      ) : (
+        <>
+          {icon}
+          <input
+            placeholder={!!error ? error : placeholder}
+            //   {...register(name)}
+            {...rest}
+            type={type}
+          />
+        </>
+      )}
+    </StyledContainer>
+  );
+};
