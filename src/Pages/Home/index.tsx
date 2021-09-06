57e97e0b257c6206dc2df660a65ae799ab61e165
Leandro <legato_soares@hotmail.com>
2021-09-06 12:02:59 -0300

    feat: Adicionado Pagina principal

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,4 +1,22 @@
+import LandingPage from "./../../Assets/Images/LandingPage.svg"
+import { Container } from "./styles";
+import { Button } from "../../Components/Button";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <figure>
+        <img src={LandingPage} alt="landingPageImage" />
+      </figure>
+    <h1>Aplicação</h1>
+    <p>Bem vindo ao [aplicação], a sua plataforma de saúde, aqui você busca profissionais, marca consultas e realiza atendimentos.
+      Para continuar, selecione abaixo.</p>
+    <div>
+      <Button>Logar</Button>
+      <p>Ou</p>
+      <Button GreenTheme> Registrar </Button>
+    </div>
+  </Container>
+  );
 };
 export default Home;
