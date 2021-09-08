877f5d2bf1681916f658fcc4105c4620ab143f2f
marcos <marcosmvg77@gmail.com>
2021-09-08 12:13:28 -0300

    feature(calendar): calendario iniciado

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,25 +1,24 @@
-import LandingPage from "./../../Assets/Images/LandingPage.svg";
+import LandingPage from "./../../Assets/Images/LandingPage.svg"
 import { Container } from "./styles";
 import { Button } from "../../Components/Button";
 import { Background } from "../../Components/Background";
 
+
 const Home = () => {
   return (
-    <Background image={LandingPage}>
-      <Container>
-        <h1>Aplicação</h1>
-        <p>
-          Bem vindo ao [aplicação], a sua plataforma de saúde, aqui você busca
-          profissionais, marca consultas e realiza atendimentos. Para continuar,
-          selecione abaixo.
-        </p>
-        <div>
-          <Button>Logar</Button>
-          <p>Ou</p>
-          <Button GreenTheme> Registrar </Button>
-        </div>
-      </Container>
-    </Background>
+    <Background image = {LandingPage}>
+    <Container>
+    <h1>Aplicação</h1>
+    <p>Bem vindo ao [aplicação], a sua plataforma de saúde, aqui você busca profissionais, marca consultas e realiza atendimentos.
+      Para continuar, selecione abaixo.</p>
+    <div>
+      <Button>Logar</Button>
+      <p>Ou</p>
+      <Button GreenTheme> Registrar </Button>
+    </div>
+  </Container>
+  </Background>
+
   );
 };
 export default Home;
